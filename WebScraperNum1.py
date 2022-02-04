a80a71b606261f6bf49a1c89ecf5ef866f5012c5
james3roberts <james3roberts@gmail.com>
2022-02-03 20:22:18 -0700

    Working on Scraper

--- WebScraperNum1.py
+++ WebScraperNum1.py
@@ -1,8 +1,33 @@
-##Going to make a webScraper in python using beautiful soup 4
-from urllib.request import urlopen
+import imp
+import requests
 from bs4 import BeautifulSoup
+from itertools import cycle
+import csv
 
-urlToScrape="https://www.internships.com/app/search?keywords=&position-types=internship&location=colorado+Spring+colorad&context=seo&seo-mcid=-1"
-requestPage=urlopen(urlToScrape)
-pageHtml = requestPage.read()
-requestPage.close()
+##Going to make proxy so I dont get kicked
+def getProxies(inURL):
+    page = requests.get(inURL)
+    soup = BeautifulSoup(page.text,'html.parser')
+    terms = soup.find_all('tr')
+    IPs = []
+    
+    for x in range (len(terms)):
+        term = str(terms[x])
+        if '<tr><td>' in str(terms[x]):
+            pos1 = term.find('d>')+2
+            pos2 = term.find('</td')
+            pos3 = term.find('/td><td>')+9
+            pos4 = term.find('</td><td>US<')
+
+            IP = term[pos1:pos2]
+            port = term[pos3:pos4]
+            if '.' in IP and len(port)<6:
+                IPs.append(IP +":"+ port)
+                #print(IP +":"+ port)
+    return IPs
+
+    #get a proxy to use
+    proxyURL = "https://www.us-proxy.org/"
+    pxs = getProxies(proxyURL)
+    proxyPool = cycle(pxs)
+    ##time is 10:46
