b05dc37215833ef1722bc21d31a9788744c6ce57
james3roberts <james3roberts@gmail.com>
2022-02-10 20:20:09 -0700

    another day

--- HelloWorld.cc
+++ HelloWorld.cc
@@ -1,7 +1,42 @@
-//Test for c++
-#include <iostream>
+// //Test for c++
+// #include <iostream>
 
-int main () {
-	std::cout <<"Hello World!";
+// int main () {
+// 	std::cout <<"Hello World!";
+// 	return 0;
+// }
+
+
+//Starter code for a simple game tetris
+//will need to move to a new file
+#include <iostream>
+using namespace std;
+// figure out what a wstring is
+wstring tetromino[7];
+int main(){
+	//create the blocks first
+	//this should make it 4 blocks tall and 4 blocks wide
+	//Making the blocks in a string so I can see what they are 
+	tetromino[0].append(L"..x.");
+	tetromino[0].append(L"..x.");
+	tetromino[0].append(L"..x.");
+	tetromino[0].append(L"..x.");
+	//create 2nd block
+	tetromino[1].append(L"..x.");
+	tetromino[1].append(L".xx.");
+	tetromino[1].append(L".x..");
+	tetromino[1].append(L"....");
+	//create 3rd block
+	tetromino[2].append(L".x..");
+	tetromino[2].append(L".xx.");
+	tetromino[2].append(L"..x.");
+	tetromino[2].append(L"....");
+	//4th block
+	tetromino[3].append(L"....");
+	tetromino[3].append(L".xx.");
+	tetromino[3].append(L".xx.");
+	tetromino[3].append(L"....");
+	//time 3:11 should show what all of the shaper are when finished. 
+
 	return 0;
 }
--- README.txt
+++ README.txt
@@ -1,4 +1,7 @@
 Read me file for my coding every work day for 1 year. 
+Python
 The first project that I will work on is a web scraper. I am hoping that I will be able to find a way to scrape job boards and create a daily list of 
 jobs to apply to. I will be using python for this. currently it scrapes software developer internships within 25 miles of colorado springs. 
 
+C++
+Starting on a tetris game. This will use a lot on thing I have not needed to learn in school
